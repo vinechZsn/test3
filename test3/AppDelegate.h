0e5e7de1e0ef1c29ae698f7d977232f67f83b27f
hzts <k2@localhost.(none)>
2012-07-15 14:43:14 +0800

    Initial Commit

---
+++ test3/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test3
+//
+//  Created by 张少南 on 12-7-15.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
